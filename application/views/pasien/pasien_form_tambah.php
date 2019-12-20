cc05565198ac8b157f0e7a50e244a7c21aed42f2
arulxd <40177637+arulxd@users.noreply.github.com>
2019-12-20 16:03:20 +0700

    Update pasien_form_tambah.php

--- application/views/pasien/pasien_form_tambah.php
+++ application/views/pasien/pasien_form_tambah.php
@@ -53,7 +53,7 @@
 
 				<div class="card-footer">
 					<button type="submit" class="btn btn-danger"><i class="fa fa-dot-circle-o"></i> Simpan </button>
-					<a href="<?php echo site_url('https://rmappbeta.azurewebsites.net/') ?>" class="btn btn-info"><i class="fa fa-sign-out"></i>
+					<a href="<?php echo site_url('index.php') ?>" class="btn btn-info"><i class="fa fa-sign-out"></i>
 						Kembali</a>
 				</div>
 			</form>
